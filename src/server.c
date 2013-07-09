25d1cab5ebf530b61a6d406e4c2e067eb0fe4746
Hugo Gibson <hugo.gibson@roh.org.uk>
2013-07-09 13:47:14 +0100

    Say something out of the socket

--- src/server.c
+++ src/server.c
@@ -1,10 +1,19 @@
-#include <sys/types.h>
 #include <sys/socket.h>
-#include <time.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <time.h> 
+
+
 
 
 int main(int argc, char *argv[]) {
-	int listen = 0, connfd = 0;
+	int listenfd = 0, connfd = 0;
 
 	struct sockaddr_in serv_addr; 
 
@@ -17,7 +26,7 @@ int main(int argc, char *argv[]) {
 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serv_addr.sin_port = htons(5000); 
+    serv_addr.sin_port = htons(7000); 
 
     bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)); 
 
@@ -26,7 +35,15 @@ int main(int argc, char *argv[]) {
 
     //start waiting for something to come through the line
     for(;;) {
-
+    	//this naturally does very little at the moment!
+    	connfd = accept(listenfd, (struct sockaddr*)NULL, NULL); 
+        ticks = time(NULL);
+        
+        snprintf(sendBuff, sizeof(sendBuff), "%.24s\r\n", ctime(&ticks));
+        write(connfd, sendBuff, strlen(sendBuff)); 
+        
+        close(connfd);
+        sleep(1);
     }
 
 }
